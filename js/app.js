f01f492d02da152f754f3185f48828a3cc092656
Loadngg <averinj@mail.ru>
2021-12-28 13:07:15 +0300

    добавлена фавиконка и анимации

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 // Scroll Top on reload
-// $(window).on('beforeunload', function(){
-//     $(window).scrollTop(0);
-// });
+$(window).on('beforeunload', function(){
+    $(window).scrollTop(0);
+});
 
 // Smooth scroll
 const smoothLinks = document.querySelectorAll('a[href^="#"]'); 
     for (let smoothLink of smoothLinks) { 
     smoothLink.addEventListener('click', function(event) { 
     event.preventDefault(); 
-    const yOffset = -80; 
+    const yOffset = -100; 
     const id = smoothLink.getAttribute('href'); 
     const element = document.querySelector(id) 
     const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset; 
@@ -69,7 +69,6 @@ btn.onclick = function () {
     let hidden = document.getElementById('hiddenTable');
     var actualDisplay = getComputedStyle(hidden).display;
     if (actualDisplay === 'none') {
-        console.log(actualDisplay);
         hidden.style.display = 'block';
         btn.innerHTML = 'скрыть всё';
     } else {
@@ -96,4 +95,44 @@ function hideOverflow() {
         block.classList.add('hidden')
         iframe.src = "about:blank";
     }, 500);
+}
+
+// Animation
+const animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemH = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemH / animStart;
+            if (animItemH > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemH)) {
+                animItem.classList.add('show');
+            } else {
+                // animItem.classList.remove('show');
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+        scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+        scrollTop = window.scrollY || document.documentElement.scrollTop;
+        return {
+            top: rect.top + scrollTop,
+            left: rect.left + scrollLeft
+        }
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 300) 
 }
